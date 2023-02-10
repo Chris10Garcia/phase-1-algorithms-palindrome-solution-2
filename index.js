84dfe87064451b5989aa1523e688e065f279f246
Christian Garcia <bones192nd@gmail.com>
2023-02-10 11:01:56 -0500

    lab submission

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  for (let i =0; i < word.length / 2; i++){
+    const j = word.length - 1 - i
+
+    if (word[i] !== word[j]){
+      return false
+    }
+  }
+
+  return true
 }
 
 /* 
-  Add your pseudocode here
+  tests if word forward is the same as word reversed
+  a string is just an array of letters
+
+  l e t t e r s
+  0 1 2 3 4 5 6
+  i           
+            j
+    i        
+          j
+      i
+        j
+
+  r a c e c a r
+  0 1 2 3 4 5 6
+  i           
+              j
+    i     
+            j
+      i
+          j
+        i
+        j
+  use for (i) iterator and use a j tracker
+  you don't need to iterate the entire length of the array, just up to the half point
+
+  so word.length /2
+
+  to get i = 0 and j = 6
+  j = word.length -1
+
+  to get j to move along i
+
+  j = word.length - i
+
 */
 
 /*
